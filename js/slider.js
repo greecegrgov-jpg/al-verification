ce5cb57ce3c0e6bda11bc56ba85b09f4afda07c9
greecegrgov-jpg <greece.gr.gov@gmail.com>
2026-08-23 05:18:41 +0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const slides = document.querySelectorAll(".swiper-slide");
+    const bullets = document.querySelectorAll(".swiper-pagination-bullet");
+
+    if (!slides.length) return;
+
+    let currentSlide = 0;
+    let timer;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = "block";
+                slide.style.opacity = "1";
+            } else {
+                slide.style.display = "none";
+                slide.style.opacity = "0";
+            }
+        });
+
+        bullets.forEach((bullet, i) => {
+            bullet.classList.toggle(
+                "swiper-pagination-bullet-active",
+                i === index
+            );
+        });
+
+        currentSlide = index;
+    }
+
+    function nextSlide() {
+        let next = currentSlide + 1;
+
+        if (next >= slides.length) {
+            next = 0;
+        }
+
+        showSlide(next);
+    }
+
+    function startSlider() {
+        timer = setInterval(nextSlide, 4000);
+    }
+
+    function stopSlider() {
+        clearInterval(timer);
+    }
+
+    bullets.forEach((bullet, index) => {
+        bullet.addEventListener("click", function () {
+            stopSlider();
+            showSlide(index);
+            startSlider();
+        });
+    });
+
+    showSlide(0);
+    startSlider();
+});
